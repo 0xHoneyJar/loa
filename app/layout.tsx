6528e37306d5508e5b74a22834fa0bfc52d84b52
wang4621 <82539201+wang4621@users.noreply.github.com>
2024-06-28 12:52:43 -0700

    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,9 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      className={`${switzer.variable}`}
-    >
+    <html className={`${switzer.variable}`}>
       <head></head>
       <body>
         {/* <Web3Provider> */}
