ce3bda8e469717b8bdf35324fd655079cbe3852b
treblale <albertphongle@gmail.com>
2024-08-11 13:07:54 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,30 @@ module.exports = {
       { hostname: "d163aeqznbc6js.cloudfront.net" },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/mint",
+        destination: "https://app.0xhoneyjar.xyz/mint",
+        permanent: true,
+      },
+      {
+        source: "/mint/berasvsapes",
+        destination: "https://app.0xhoneyjar.xyz/mint/berasvsapes",
+        permanent: true,
+      },
+      {
+        source: "/mint/miladysandberas",
+        destination: "https://app.0xhoneyjar.xyz/mint/miladysandberas",
+        permanent: true,
+      },
+      {
+        source: "/mint/pudgyfrens",
+        destination: "https://app.0xhoneyjar.xyz/mint/pudgyfrens",
+        permanent: true,
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
